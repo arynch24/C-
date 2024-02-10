fa751bd4bb0cd250d71b74f5cb0af0f414a1dc34
arynch24 <arynch24@gmail.com>
2024-02-10 23:55:14 +0530

    commit

---
+++ basic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello!\nMyself Aryan Chauhan";
+    cout<<endl;//enter
+    cout<<"I am a coder"<<endl;
+
+    //or
+    cout<<"Hello!\nMyself Aryan Chauhan"<<endl;//enter
+    cout<<"I am a coder";
+
+    int x=2,y=98;
+    cout<<endl<<x+y;
+}
