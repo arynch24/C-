1bd2d73c73a17a53b0d6ed0addfb7f687ed6b535
arynch24 <arynch24@gmail.com>
2024-08-05 19:35:18 +0530

    array stl

---
+++ STL/array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int basic[3]={1,2,3};
+
+    array<int,4> a={5,2,3,6};
+
+    int size =a.size();
+
+    cout<<a.at(2)<<endl;
+
+    cout<<a.empty()<<endl;
+
+    cout<<a.front()<<endl;
+
+    cout<<a.back()<<endl;
+}
