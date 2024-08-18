cc0ea5ccc1b6f5910789c1c015ab3bc072eeae97
arynch24 <arynch24@gmail.com>
2024-08-18 22:07:20 +0530

    base case 1 & 2

---
+++ Recursion/fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n == 1)
+        return 0;
+    else if (n == 2)
+        return 1;
+
+    return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << fibonacci(n) << endl;
+}
+
